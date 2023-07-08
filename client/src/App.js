79b4fb21b5bde1f9010c52f2c2b98a990517ee66
Łukasz Lewandowski <lukasz.lewandowski1@asahibeer.pl>
2023-07-08 13:01:41 +0200

    add socet

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,17 @@
+import { useEffect, useState } from "react";
+import io from "socket.io-client";
 const App = () => {
+  const [socket, setSocket] = useState("");
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    const newSocet = io(process.env.PORT || "http://localhost:8000/");
+    setSocket(newSocet);
+  }, []);
+
+  const removeTask = (id) => {
+    setTasks((tasks) => tasks.filter((task) => task.id !== id));
+  };
   return (
     <div className="App">
       <header>
@@ -9,12 +22,23 @@ const App = () => {
         <h2>Tasks</h2>
 
         <ul className="tasks-section__list" id="tasks-list">
-          <li className="task">
+          {tasks.map((task) => (
+            <li key={task.id} className="task">
+              {task.name}{" "}
+              <button
+                className="btn btn--red"
+                onClick={() => removeTask(task.id)}
+              >
+                Remove
+              </button>
+            </li>
+          ))}
+          {/* <li className="task">
             Shopping <button className="btn btn--red">Remove</button>
           </li>
           <li className="task">
             Go out with a dog <button className="btn btn--red">Remove</button>
-          </li>
+          </li> */}
         </ul>
 
         <form id="add-task-form">
--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const express = require("express");
+const path = require("path");
+const socket = require("socket.io");
 const app = express();
+
+const tasks = [];
 
 app.use((req, res) => {
   res.status(404).send({ message: "Not found..." });
@@ -7,4 +11,21 @@ app.use((req, res) => {
 
 const server = app.listen(process.env.PORT || 8000, () => {
   console.log("Server is running on port: 8000 ");
+});
+
+const io = socket(server);
+
+io.on("connection", (socket) => {
+  console.log("New client! Its id – " + socket.id);
+  io.to(socket.id).emit("updateData", tasks);
+
+  socket.on("addTask", (taskData) => {
+    tasks.push({ id: taskData.id, name: taskData.description });
+    socket.broadcast.emit("addTask");
+  });
+
+  socket.on("removeTask", (taskId) => {
+    tasks = tasks.filter((task) => task.id !== taskId);
+    socket.broadcast.emit("removeTask");
+  });
 });
